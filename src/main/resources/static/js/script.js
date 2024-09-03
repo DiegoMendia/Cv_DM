bb8acf869ff9c7e949c8375575876535961302b2
DiegoMendia <diegomendia21@gmail.com>
2024-09-03 20:59:18 -0300

    modifico seccion skills

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -21,18 +21,12 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("java");
-        habilidades[3].classList.add("sql");
-        habilidades[4].classList.add("spring");
-        habilidades[5].classList.add("php");
-        habilidades[6].classList.add("autodidacta");
-        habilidades[7].classList.add("comunicacion");
-        habilidades[8].classList.add("trabajo");
-        habilidades[9].classList.add("creatividad");
-        habilidades[10].classList.add("dedicacion");
-        habilidades[11].classList.add("tolerar");
+         habilidades[0].classList.add("autodidacta");
+                habilidades[1].classList.add("comunicacion");
+                habilidades[2].classList.add("trabajo");
+                habilidades[3].classList.add("creatividad");
+                habilidades[4].classList.add("dedicacion");
+                habilidades[5].classList.add("tolerar");
     }
 }
 
